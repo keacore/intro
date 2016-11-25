15afe73c0294818f24f3cf49a417138c370703bd
clbokea <clbo@kea.dk>
2016-11-25 13:22:22 +0100

    EF exercise added

---
+++ exercise_ef/Migrations/20161124104459_MyFirst.Designer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ConsoleApplication;
+
+namespace efClass.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20161124104459_MyFirst")]
+    partial class MyFirst
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("ConsoleApplication.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("ConsoleApplication.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("ConsoleApplication.Post", b =>
+                {
+                    b.HasOne("ConsoleApplication.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ exercise_ef/readme.md
@@ -0,0 +1,4 @@
+# Exercise EF (Entity Framework)
+## Solution to .NET Core - New Database
+This is the solution for the tutorial/Exercise that can be found at 
+[.NET Core - New Database](https://docs.microsoft.com/en-us/ef/core/get-started/netcore/new-db-sqlite)
